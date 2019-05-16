ca87ed0673f761741a2ff1bea94ff93c5080955c
mamonovvlad <vladik301096>
2019-05-16 17:39:24 +0300

    ghch

--- src/js/script.js
+++ src/js/script.js
@@ -21,10 +21,74 @@ $(".burger-menu").on("click" , function () {
 
 $(".modal_switch").on("click" , function () {
 	$(".background-styles-modal").fadeIn();
+	$(".modal_switch").fadeOut();
 })
 
 $(".background-styles-modal").on("click" , function () {
 	$(this).fadeOut();
+	$(".modal_switch").fadeIn();
+
 })
+
+$(".click").click(function (e) {
+	e.preventDefault();
+	$(".block-two").toggleClass('d-b');
+	$("#loft .box-loft .block-one .loft").toggleClass('activ')
+	$(".block-two").css({'transition' : '500ms ease'});
+	$(".click").css({'transition' : '500ms ease'});
+	
+})
+$("#loft .box-loft .click p").click(function () {
+	$(".block-two").toggleClass('d-b');
+	$("#loft .box-loft .block-one .loft").toggleClass('activ')
+	$(".block-two").css({'transition' : '500ms ease'});
+	$(".click_p").css({'transition' : '500ms ease'});
+
+})
+
 $('.single-item').slick();
+
+function getWindowWidth() {
+	return window.innerWidth || document.body.clientWidth;
+ }
+ 
+ if (getWindowWidth() <= 768) {
+ 
+ 
+		 / переменная равна 1 /
+		 let marker = true;
+ 
+ 
+		 /* срабатывает функция и переменная marker
+		 еняет значение на 0 */
+		 function count() {
+			  appear_modal(".background-styles-modal");
+			  marker = false;
+		 }
+ 
+ 
+		 function appear_modal(appear_element)
+		 {
+			 $(appear_element).fadeIn();
+ 
+		 }
+ 
+			 $(window).on("scroll",function()
+			 {
+				 /*  сама функция сработает только при условии
+				 того что marker == 1 */
+			 if ( $(window).scrollTop() >=  1070) { 
+					if ( marker ) {
+						 count();
+					}	
+			 }
+			 
+			 })
+ }
+
+
+
+
+
+
 })
